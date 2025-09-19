99038a0e42f82255efc132e36386bdd76fe91d28
Utopianboy99 <123115444+Utopianboy99@users.noreply.github.com>
2025-09-19 23:24:39 +0200

    Update AboutUs.jsx

--- Cognition_Berries/src/pages/AboutUs/AboutUs.jsx
+++ Cognition_Berries/src/pages/AboutUs/AboutUs.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
-import './Aboutus.css'
+import './AboutUs.css'
 import Footer from '../../components/Footer/Footer'
 
 
